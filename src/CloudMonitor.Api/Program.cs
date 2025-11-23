8017e55301ce0e4ae18187190f0ef6daf998d711
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-11-23 21:03:18 +0100

    add Application Insights telemetry support to CloudMonitor API

--- src/CloudMonitor.Api/Program.cs
+++ src/CloudMonitor.Api/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
